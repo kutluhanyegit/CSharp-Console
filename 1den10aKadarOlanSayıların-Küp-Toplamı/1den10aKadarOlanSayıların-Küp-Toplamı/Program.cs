d34a2ce1ab348753b9a7bf82f6cf13f15ef6818d
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 16:28:24 +0300

    1den10 a kadar olan Sayıların Küp Toplamları

---
+++ 1den10aKadarOlanSayıların-Küp-Toplamı/1den10aKadarOlanSayıların-Küp-Toplamı/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1den10aKadarOlanSayıların_Küp_Toplamı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1den10 a kadar olan sayıların küp toplamı
+            double toplam = 0;
+
+            for (int i = 1; i < 11; i++)
+            {
+                toplam +=Math.Pow(i, 3);
+            }
+            Console.Write("1den10 a kadar Olan sayıların küplerinin Toplamı = " + toplam);
+            Console.ReadKey();
+        }
+    }
+}
