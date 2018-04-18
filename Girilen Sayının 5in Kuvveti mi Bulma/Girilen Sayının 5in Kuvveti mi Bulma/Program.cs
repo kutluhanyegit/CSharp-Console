ca13d3570d3d8737da9e34a428207d06db281255
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 21:06:52 +0300

    Girilen Sayının 5in Kuvveti Olup Olmadığını Bulma

---
+++ Girilen Sayının 5in Kuvveti mi Bulma/Girilen Sayının 5in Kuvveti mi Bulma/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Girilen_Sayının_5in_Kuvveti_mi_Bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen Sayı 5in Kuvveti mi Bulma
+            Console.Write("Sayı Girin : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sayac = 0;
+
+            while (true)
+            {
+                if (sayi%5==0)
+                {
+                    sayac++;
+                    sayi /= 5;
+                    if (sayi==1)
+                    {
+                        Console.WriteLine("5'in "+sayac +". Kuvveti");
+                        
+                        break;
+                    }
+                     
+                }
+                else
+                {
+                    Console.WriteLine("Değil");
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
