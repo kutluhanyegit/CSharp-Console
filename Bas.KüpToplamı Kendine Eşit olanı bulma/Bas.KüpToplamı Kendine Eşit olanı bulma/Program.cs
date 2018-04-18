efd2228aa335c45b0efcf8ea57615ce457c58163
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 17:29:51 +0300

    Basamak Küp Toplamı Kendine Eşit mi Değil mi ? bulan Program

---
+++ Bas.KüpToplamı Kendine Eşit olanı bulma/Bas.KüpToplamı Kendine Eşit olanı bulma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bas.KüpToplamı_Kendine_Eşit_olanı_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayıyı Girin : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            double toplam = 0;
+            int y;
+            y = sayi;
+            while (y > 0)
+            {
+                toplam += Math.Pow((y % 10), 3);
+                y /= 10;
+            }
+            Console.WriteLine("Basamaklarının Küpleri Toplamı : " + toplam);
+
+            if (sayi==toplam)
+            {
+                Console.WriteLine("Basamak Küp Toplamı Kendisine Eşittir!!");
+            }
+            else
+            {
+                Console.WriteLine("Basamak Küp Toplamı Kendisine Eşit Değildir!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
