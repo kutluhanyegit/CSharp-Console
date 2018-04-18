82b73ef74bcfa4fac95d63c3a4f98c546f8809b0
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 20:06:11 +0300

    Girilen 10 Adet Sayıdan Poz Neg Oranı

---
+++ Girilen10Adet sayıdan poz Neg Oran bulma/Girilen10Adet sayıdan poz Neg Oran bulma/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Girilen10Adet_sayıdan_poz_Neg_Oran_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen 10 Adet Sayıdan Pozitiflerin Negatiflere Oranı
+            int sayi, pozToplam = 0, negToplam = 0;
+            double oran;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write((i + 1) + ". Değeri Girin : ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi>0)
+                {
+                    pozToplam += sayi;   
+                }
+                if (sayi<0)
+                {
+                    negToplam += sayi;
+                }
+            }
+            oran = pozToplam / negToplam;
+            Console.WriteLine("Girilen Değerlerin Oranı : "+oran);
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Girilen10Adet sayıdan poz Neg Oran bulma/Girilen10Adet sayıdan poz Neg Oran bulma/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Girilen10Adet sayıdan poz Neg Oran bulma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Girilen10Adet sayıdan poz Neg Oran bulma")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("77e60b16-1016-4e69-81c4-ed262b3a3dff")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
