9d40f8435c184db8b35e136a94d868ccb4ca009b
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 16:50:44 +0300

    Girilen Sayının Faktoriyelini Bulma

---
+++ Girilen Sayının Faktoriyeli/Girilen Sayının Faktoriyeli/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Girilen_Sayının_Faktoriyeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Faktoriyelini Almak İstediğiniz Sayıyı Girin : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int faktoriyel = 1; //Çarpma işlemi olcağı için 1 ataması yaptım
+            for (int i = 1; i <=sayi; i++)
+            {
+                faktoriyel *= i;
+            }
+            Console.WriteLine("Girilen Sayının Faktoriyeli : " + faktoriyel);
+            Console.ReadKey();
+        }
+    }
+}
