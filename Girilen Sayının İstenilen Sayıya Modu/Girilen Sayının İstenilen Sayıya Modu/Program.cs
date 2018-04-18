a3f99cf433cfabbf24fe8f378571b8d14fc63ac6
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 17:11:22 +0300

    Girilen Sayının İstenilen Sayıya Modu

---
+++ Girilen Sayının İstenilen Sayıya Modu/Girilen Sayının İstenilen Sayıya Modu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Girilen_Sayının_İstenilen_Sayıya_Modu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Modunu Almak İstediğiniz Sayıyı Girin : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kaça Mod almak istiyorsunuz : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sonuc = a % b;
+            Console.Write("Mod İşleminin Sonuc : " + sonuc);
+            Console.ReadKey();
+        }
+    }
+}
