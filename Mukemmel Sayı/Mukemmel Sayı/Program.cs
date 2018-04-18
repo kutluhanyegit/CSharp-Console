74211e24d1db33c73ac901534449bacdcb5a3fbd
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 21:38:49 +0300

    Mükemmel Sayı

---
+++ Mukemmel Sayı/Mukemmel Sayı/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mukemmel_Sayı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayıyı Girin : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+
+            for (int i = 1; i < a; i++)
+            {
+                if (a%i==0)
+                {
+                    toplam += i;
+                }
+            }
+
+            if (toplam==a)
+            {
+                Console.WriteLine("Mükemmel Sayı");
+            }
+            else
+            {
+                Console.WriteLine("Mükemmel Sayı Değil");
+            }
+            Console.ReadKey();
+        }
+    }
+}
