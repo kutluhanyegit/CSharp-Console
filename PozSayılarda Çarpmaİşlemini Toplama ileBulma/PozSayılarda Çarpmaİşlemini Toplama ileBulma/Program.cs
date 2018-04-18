6e34a654254f6c6020d302b5169614d2c71328bd
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 16:55:57 +0300

    Poz. Sayılarda Çarpma İşlemi Sonucunu Toplama Yaparak Bulma

---
+++ PozSayılarda Çarpmaİşlemini Toplama ileBulma/PozSayılarda Çarpmaİşlemini Toplama ileBulma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PozSayılarda_Çarpmaİşlemini_Toplama_ileBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Çarpmak İStediğiniz ilk Değeri Girin : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Çarpmak İstediğiniz ikinci Değeri Girin : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sonuc = 0;
+            for (int i = 0; i <b; i++)
+            {
+                sonuc += a;
+            }
+            Console.Write("Girilen Değerlerin Sonuc : " + sonuc);
+            Console.ReadKey();
+        }
+    }
+}
