3321f26585f5605a12a0219caa607e4a376c3abe
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 21:31:14 +0300

    Fibonacci ilk 10 Terimi

---
+++ Fibonacci Serisi/Fibonacci Serisi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Serisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[10];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+            Console.WriteLine("1. Terimi "+fibonacci[0]);
+            Console.WriteLine("2. Terimi "+fibonacci[1]);
+            for (int i = 2; i < 10; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                Console.WriteLine((i+1)+". Terimi "+fibonacci[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
