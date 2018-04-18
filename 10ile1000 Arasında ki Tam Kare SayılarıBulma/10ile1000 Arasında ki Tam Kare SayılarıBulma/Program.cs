445a89eddfea0703f733c72a5e9205db06554595
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 20:12:48 +0300

    10-1000 Arasında ki Tam Kare Sayıları Bulma

---
+++ 10ile1000 Arasında ki Tam Kare SayılarıBulma/10ile1000 Arasında ki Tam Kare SayılarıBulma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10ile1000_Arasında_ki_Tam_Kare_SayılarıBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10 ile 1000 arasında ki tam kare Sayılar
+            //1 2 3 kareleri 10dan kucuk oldugu için saymıyoruz
+
+            double sayi = 4;
+            while (Math.Pow(sayi,2)<=1000)
+            {
+                Console.WriteLine(sayi + " Tam Kare Koşulunu Sağlar");
+                sayi++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
