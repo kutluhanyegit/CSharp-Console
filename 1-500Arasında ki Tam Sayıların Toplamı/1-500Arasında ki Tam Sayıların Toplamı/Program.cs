4bfb3e6bdf81cde9e3f63a757bbc54c4c97960ff
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 22:19:06 +0300

    1-500 Arasında ki Tam Sayıların Toplamı

---
+++ 1-500Arasında ki Tam Sayıların Toplamı/1-500Arasında ki Tam Sayıların Toplamı/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_500Arasında_ki_Tam_Sayıların_Toplamı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            for (int i = 1; i <= 500; i++)
+            {
+                toplam += i;
+            }
+            Console.WriteLine("Toplamları : " + toplam);
+            Console.ReadKey();
+        }
+    }
+}
