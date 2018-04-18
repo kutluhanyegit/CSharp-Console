5d203e7d934c37509d528447449a9d94f469ed30
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 17:16:08 +0300

    Kaç Basamaklı Oldugunu Bulma

---
+++ Kaç Basamaklı Oldugunu Bulma/Kaç Basamaklı Oldugunu Bulma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaç_Basamaklı_Oldugunu_Bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayıyı Girin : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int basamaksayısı = 0;
+            while (sayi > 0)
+            {
+                sayi /= 10;
+                basamaksayısı++;
+            }
+            Console.Write("Girilen Sayı "+basamaksayısı + " basamaklıdır!");
+            Console.ReadKey();
+        }
+    }
+}
