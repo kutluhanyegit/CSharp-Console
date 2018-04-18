f8c2c77f7074366b09be30ee2c2544fa4487575b
K <35039393+kutluhanyegit@users.noreply.github.com>
2018-04-18 16:47:21 +0300

    Doğum Yılı Girilen Kişinin Yaşını Hesaplama

---
+++ Doğum Tarihi Girilen Kişinin Yaşını Bulma/Doğum Tarihi Girilen Kişinin Yaşını Bulma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doğum_Tarihi_Girilen_Kişinin_Yaşını_Bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Doğum Yılınızı Giriniz : ");
+            int dy = Convert.ToInt32(Console.ReadLine());
+            int yas = DateTime.Now.Year - dy;
+            Console.Write("Yaşınız : " + yas);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Doğum Tarihi Girilen Kişinin Yaşını Bulma/Doğum Tarihi Girilen Kişinin Yaşını Bulma/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Doğum Tarihi Girilen Kişinin Yaşını Bulma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Doğum Tarihi Girilen Kişinin Yaşını Bulma")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("bc5789b0-9a4a-4fc0-848b-bb07642cb101")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
